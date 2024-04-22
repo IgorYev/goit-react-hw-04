838041fbe6233604d679b8ea8432f47e2eaceb9e
IgorYev <123358377+IgorYev@users.noreply.github.com>
2024-04-22 09:59:12 +0300

    1
    Commit1

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -3,7 +3,7 @@ import css from "./ErrorMessage.module.css";
 export default function ErrorMessage() {
   return (
     <div className={css.section}>
-      <p>Oops! Something went wrong... </p>
+      <p>Oops! Something went wrong! </p>
     </div>
   );
 }
